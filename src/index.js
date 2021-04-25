5c41c090882758bc8fc28530f969573067786c2d
mt1221 <77732827+mt1221@users.noreply.github.com>
2021-04-25 12:25:28 +0900

    デフォルト値

--- src/index.js
+++ src/index.js
@@ -86,27 +86,34 @@
 // const func3 = (num1, num2) => num1 + num2;
 // console.log(func3(10, 2));
 
+// /**
+//  * 分館つ代入
+//  */
+
+// // const myProfile = {
+// //   name: "じゃけぇ",
+// //   age: 28
+// // };
+
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// // console.log(message1);
+
+// // const { name, age } = myProfile;
+// // const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// // console.log(message2);
+
+// const myProfile = ["じゃけぇ", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}最です。`;
+// console.log(message4);
+
 /**
- * 分館つ代入
+ * デフォルト値,　引数など
  */
 
-// const myProfile = {
-//   name: "じゃけぇ",
-//   age: 28
-// };
-
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-// console.log(message1);
-
-// const { name, age } = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}歳です。`;
-// console.log(message2);
-
-const myProfile = ["じゃけぇ", 28];
-
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
-
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}最です。`;
-console.log(message4);
+const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+sayHello("じゃけぇ");
