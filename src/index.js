c59cd6949a0f7853442c859d1cad01c5581d8173
mt1221 <77732827+mt1221@users.noreply.github.com>
2021-04-25 12:23:10 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -61,27 +61,52 @@
 // const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
 
+// /**
+//  * アロー関数
+//  */
+
+// //従来の関数
+// // function func1(str) {
+// //   return str;
+// // }
+
+// const func1 = function (str) {
+//   return str;
+// };
+
+// console.log(func1("func1です"));
+
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 2));
+
 /**
- * アロー関数
+ * 分館つ代入
  */
 
-//従来の関数
-// function func1(str) {
-//   return str;
-// }
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
 
-const func1 = function (str) {
-  return str;
-};
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
 
-console.log(func1("func1です"));
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
 
-//アロー関数
-const func2 = (str) => {
-  return str;
-};
+const myProfile = ["じゃけぇ", 28];
 
-console.log(func2("func2です"));
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 2));
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}最です。`;
+console.log(message4);
